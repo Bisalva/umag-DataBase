8f004b8f27b0fe8ad7d9bf16ee81294107e7d980
Bisalva <bisalva99@gmail.com>
2025-08-21 16:37:50 -0300

    feat : add found item example

--- 00-classwork/types.py
+++ 00-classwork/types.py
@@ -11,3 +11,9 @@ def send_hello(name: str | None = None)  -> str:
     print("Hola")
 
 send_hello(["Diego","Alan"])
+
+def found_item(lista : list[int], elemento : int) -> int | None :
+    if elemento in lista:
+        return lista.index(elemento)
+
+print(found_item([1,2,3,4,5,6],4))
