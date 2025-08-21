d7d8025fbcc3a57d7b8f74ee77219c438e0ee8c2
Bisalva <bisalva99@gmail.com>
2025-08-21 16:34:58 -0300

    feat : add list example

--- 00-classwork/types.py
+++ 00-classwork/types.py
@@ -3,7 +3,11 @@ edad : int = 12
 
 def send_hello(name: str | None = None)  -> str:
     if name is not None:
-        return f"Hola,{name}"
-    return "Hola"
+        if isinstance(name,list):
+            for n in name:
+                print(f"Hala {n}")
+            return
+        print(f"Hola,{name}")
+    print("Hola")
 
-print(send_hello())
+send_hello(["Diego","Alan"])
