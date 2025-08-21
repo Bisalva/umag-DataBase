1a03cf61a46ccd88e6941caaca819355a07489c6
Bisalva <bisalva99@gmail.com>
2025-08-21 16:45:31 -0300

    feat : add results metod

--- 00-classwork/types.py
+++ 00-classwork/types.py
@@ -17,3 +17,15 @@ def found_item(lista : list[int], elemento : int) -> int | None :
         return lista.index(elemento)
 
 print(found_item([1,2,3,4,5,6],4))
+
+
+def avegare_results(res : dict[str,int | float]) -> int | float :
+    add = sum(res.values())
+    return add / len(res)
+
+print(
+    avegare_results({
+    "pedro" : 12,
+    "juan" : 10,
+    "diego" : 20,
+}))
